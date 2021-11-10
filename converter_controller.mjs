44637a125f582ef0fb30a9116d37917a812c91e1
tyler <tylerchin92@gmail.com>
2021-11-10 10:18:54 -0700

    added cors dependency

--- converter_controller.mjs
+++ converter_controller.mjs
@@ -1,11 +1,13 @@
 'use strict';
 import express from 'express';
+import cors from 'cors'
 
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 
 app.get('/convert/:from/:to/:value', (req, res) => {
